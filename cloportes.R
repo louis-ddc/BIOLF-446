250f10d426487d6bce01be0db4e6e85c10f014c7
louis-ddc <95404225+louis-ddc@users.noreply.github.com>
2021-12-02 09:01:08 +0100

    Add files via upload

---
+++ cloportes.R
@@ -0,0 +1,67 @@
+#Question 1 binôme
+data = data.frame(matrix(nrow=4, ncol=2))
+colnames(data) = c("quartiers","présence")
+data$quartiers = as.factor(c("AC","AF","QH",
+                                "QS"))
+data$présence = c(3, 3, 1, 1)
+frequences_attendues = c(1/4, 1/4, 1/4, 1/4)
+valeurs_attendues = sum(data$présence)*frequences_attendues
+chi2 = sum(((data$présence-valeurs_attendues)^2)/valeurs_attendues)
+ddl = length(data$quartiers)-1
+pchisq(chi2, ddl, lower.tail=FALSE)
+
+
+#Question 1 classe 
+data = data.frame(matrix(nrow=4, ncol=2))
+colnames(data) = c("quartiers","présence")
+data$quartiers = as.factor(c("AC","AF","QH",
+                             "QS"))
+data$présence = c(51,	43,	34,	22)
+frequences_attendues = c(1/4, 1/4, 1/4, 1/4)
+valeurs_attendues = sum(data$présence)*frequences_attendues
+chi2 = sum(((data$présence-valeurs_attendues)^2)/valeurs_attendues)
+ddl = length(data$quartiers)-1
+pchisq(chi2, ddl, lower.tail=FALSE)
+
+#Question 2.1 binôme 
+
+data = data.frame(matrix(nrow=4, ncol=2))
+colnames(data) = c("quartiers","présence")
+data$quartiers = as.factor(c("AC","AF","QH",
+                             "QS"))
+data$présence = c(2/8,	19/8,	41/8,	2/8) #/8 c'est avec les moyennes 
+frequences_attendues = c(1/4, 1/4, 1/4, 1/4)
+valeurs_attendues = sum(data$présence)*frequences_attendues
+chi2 = sum(((data$présence-valeurs_attendues)^2)/valeurs_attendues)
+ddl = length(data$quartiers)-1
+pchisq(chi2, ddl, lower.tail=FALSE)
+
+#Quesiton 2.2 binome
+
+data = data.frame(matrix(nrow=4, ncol=2))
+colnames(data) = c("quartiers","présence")
+data$quartiers = as.factor(c("AC","AF","QH",
+                             "QS"))
+data$présence = c(0, 3, 4, 1)
+frequences_attendues = c(1/4, 1/4, 1/4, 1/4)
+valeurs_attendues = sum(data$présence)*frequences_attendues
+chi2 = sum(((data$présence-valeurs_attendues)^2)/valeurs_attendues)
+ddl = length(data$quartiers)-1 
+pchisq(chi2, ddl, lower.tail=FALSE)
+
+#Quesiton 2.2 classe
+
+data = data.frame(matrix(nrow=4, ncol=2))
+colnames(data) = c("quartiers","présence")
+data$quartiers = as.factor(c("AC","AF","QH",
+                             "QS"))
+data$présence = c(47,	51,	39,	20)
+frequences_attendues = c(1/4, 1/4, 1/4, 1/4)
+valeurs_attendues = sum(data$présence)*frequences_attendues
+chi2 = sum(((data$présence-valeurs_attendues)^2)/valeurs_attendues)
+ddl = length(data$quartiers)-1 
+pchisq(chi2, ddl, lower.tail=FALSE)
+
+
+
+
