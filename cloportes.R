196bec407df976a9b402eda790f3d8dbec616d46
louis-ddc <95404225+louis-ddc@users.noreply.github.com>
2021-12-06 19:46:21 +0100

    Update cloportes.R

--- cloportes.R
+++ cloportes.R
@@ -1,67 +1,78 @@
-#Question 1 binôme
+Code couleur pour les diffÃ©rents quartiers :
+skyblue = AC
+palegreen3 = AF 
+mediumvioletred = QH
+yellow2 = QS 
+
+#Question 1 binÃ´me
 data = data.frame(matrix(nrow=4, ncol=2))
-colnames(data) = c("quartiers","présence")
+colnames(data) = c("quartiers","prÃ©sence")
 data$quartiers = as.factor(c("AC","AF","QH",
                                 "QS"))
-data$présence = c(3, 3, 1, 1)
+data$prÃ©sence = c(3, 3, 1, 1)
 frequences_attendues = c(1/4, 1/4, 1/4, 1/4)
-valeurs_attendues = sum(data$présence)*frequences_attendues
-chi2 = sum(((data$présence-valeurs_attendues)^2)/valeurs_attendues)
+valeurs_attendues = sum(data$prÃ©sence)*frequences_attendues
+chi2 = sum(((data$prÃ©sence-valeurs_attendues)^2)/valeurs_attendues)
 ddl = length(data$quartiers)-1
 pchisq(chi2, ddl, lower.tail=FALSE)
 
 
 #Question 1 classe 
 data = data.frame(matrix(nrow=4, ncol=2))
-colnames(data) = c("quartiers","présence")
+colnames(data) = c("quartiers","prÃ©sence")
 data$quartiers = as.factor(c("AC","AF","QH",
                              "QS"))
-data$présence = c(51,	43,	34,	22)
+data$prÃ©sence = c(51,	43,	34,	22)
 frequences_attendues = c(1/4, 1/4, 1/4, 1/4)
-valeurs_attendues = sum(data$présence)*frequences_attendues
-chi2 = sum(((data$présence-valeurs_attendues)^2)/valeurs_attendues)
+valeurs_attendues = sum(data$prÃ©sence)*frequences_attendues
+chi2 = sum(((data$prÃ©sence-valeurs_attendues)^2)/valeurs_attendues)
 ddl = length(data$quartiers)-1
 pchisq(chi2, ddl, lower.tail=FALSE)
 
-#Question 2.1 binôme 
+#Question 2.1 binÃ´me 
 
 data = data.frame(matrix(nrow=4, ncol=2))
-colnames(data) = c("quartiers","présence")
+colnames(data) = c("quartiers","prÃ©sence")
 data$quartiers = as.factor(c("AC","AF","QH",
                              "QS"))
-data$présence = c(2/8,	19/8,	41/8,	2/8) #/8 c'est avec les moyennes 
+data$prÃ©sence = c(2/8,	19/8,	41/8,	2/8) #/8 c'est avec les moyennes 
 frequences_attendues = c(1/4, 1/4, 1/4, 1/4)
-valeurs_attendues = sum(data$présence)*frequences_attendues
-chi2 = sum(((data$présence-valeurs_attendues)^2)/valeurs_attendues)
+valeurs_attendues = sum(data$prÃ©sence)*frequences_attendues
+chi2 = sum(((data$prÃ©sence-valeurs_attendues)^2)/valeurs_attendues)
 ddl = length(data$quartiers)-1
 pchisq(chi2, ddl, lower.tail=FALSE)
 
 #Quesiton 2.2 binome
 
 data = data.frame(matrix(nrow=4, ncol=2))
-colnames(data) = c("quartiers","présence")
+colnames(data) = c("quartiers","prÃ©sence")
 data$quartiers = as.factor(c("AC","AF","QH",
                              "QS"))
-data$présence = c(0, 3, 4, 1)
+data$prÃ©sence = c(0, 3, 4, 1)
 frequences_attendues = c(1/4, 1/4, 1/4, 1/4)
-valeurs_attendues = sum(data$présence)*frequences_attendues
-chi2 = sum(((data$présence-valeurs_attendues)^2)/valeurs_attendues)
+valeurs_attendues = sum(data$prÃ©sence)*frequences_attendues
+chi2 = sum(((data$prÃ©sence-valeurs_attendues)^2)/valeurs_attendues)
 ddl = length(data$quartiers)-1 
 pchisq(chi2, ddl, lower.tail=FALSE)
 
 #Quesiton 2.2 classe
 
 data = data.frame(matrix(nrow=4, ncol=2))
-colnames(data) = c("quartiers","présence")
+colnames(data) = c("quartiers","prÃ©sence")
 data$quartiers = as.factor(c("AC","AF","QH",
                              "QS"))
-data$présence = c(47,	51,	39,	20)
+data$prÃ©sence = c(47,	51,	39,	20)
 frequences_attendues = c(1/4, 1/4, 1/4, 1/4)
-valeurs_attendues = sum(data$présence)*frequences_attendues
-chi2 = sum(((data$présence-valeurs_attendues)^2)/valeurs_attendues)
+valeurs_attendues = sum(data$prÃ©sence)*frequences_attendues
+chi2 = sum(((data$prÃ©sence-valeurs_attendues)^2)/valeurs_attendues)
 ddl = length(data$quartiers)-1 
 pchisq(chi2, ddl, lower.tail=FALSE)
 
+col=c("cadetblue3","aquamarine3","coral3","darkgoldenrod3","cadetblue3","aquamarine3","coral3","darkgoldenrod3")
+boxplot(frequence~sexe.quartier, data=exp1.1,
+        main="FrÃ©quence d'observation des individus mÃ¢le et femelle en fonction du quartier",
+        col=c("skyblue","palegreen3","mediumvioletred","yellow2")
+        )
 
 
 
